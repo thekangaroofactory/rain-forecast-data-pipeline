a737a6ac0059d07a04e8d6351f3ebdfdd69e559b
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-04-19 13:20:32 +0200

    Fixed #11

--- R/prediction/prediction_pipeline.R
+++ R/prediction/prediction_pipeline.R
@@ -1,6 +1,6 @@
 
 
-prediction_pipeline <- function(functional, incremental = TRUE){
+prediction_pipeline <- function(functional, incremental = TRUE, simulation = FALSE){
 
   cat("Starting prediction pipeline \n")
   
@@ -8,11 +8,12 @@ prediction_pipeline <- function(functional, incremental = TRUE){
   predictions <- make_predictions(name = "M1", functional)
 
   # -- import / update db
-  import <- db_import("prediction", predictions)
+  if(!simulation)
+    import <- db_import("prediction", predictions)
   
   # -- keep only imported rows (create / update)
-  # unless incremental is turned off
-  if(incremental)
+  # unless both incremental and simulation are turned off
+  if(incremental && !simulation)
     predictions[predictions$observation_id %in% import$row_ids, ]
   else
     predictions
