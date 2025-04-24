332b5ac404edfcab19fe3b78e60d351474a924d9
thekangaroofactory <67638275+thekangaroofactory@users.noreply.github.com>
2025-04-24 18:35:04 +0200

    Fixed #7

--- launch_api.R
+++ launch_api.R
@@ -1,5 +1,55 @@
 
 library(plumber)
 
+
+# ------------------------------------------------------------------------------
+# Authentication
+# ------------------------------------------------------------------------------
+
+# -- Helper function:
+# Adds authentication to a given set of paths 
+# (if paths = NULL, apply to all)
+add_auth <- function(x, paths = NULL) {
+  
+  # -- Adds the components element to openapi (specifies auth method)
+  x[["components"]] <- list(
+    securitySchemes = list(
+      ApiKeyAuth = list(
+        type = "apiKey",
+        `in` = "header",
+        name = "X-API-KEY",
+        description = "Here goes something")))
+  
+  # -- Add the security element to each path
+  # Eg add the following yaml to each path
+  # ...
+  # paths:
+  #   /ping:
+  #     get:
+  #       security:               #< THIS
+  #         - ApiKeyAuth: []      #< THIS
+  
+  if (is.null(paths)) paths <- names(x$paths)
+  for (path in paths) {
+    nn <- names(x$paths[[path]])
+    for (p in intersect(nn, c("get", "head", "post", "put", "delete"))) {
+      x$paths[[path]][[p]] <- c(
+        x$paths[[path]][[p]],
+        list(security = list(list(ApiKeyAuth = vector()))))}}
+  
+  # -- return
+  x
+  
+}
+
+
+# ------------------------------------------------------------------------------
+# Launch API
+# ------------------------------------------------------------------------------
+
 pr("plumber.R") %>%
+  # -- add the authentication
+  pr_set_api_spec(add_auth) %>%  
+  # -- run the API
   pr_run(port = 10000, host = "0.0.0.0")
+
--- plumber.R
+++ plumber.R
@@ -19,7 +19,13 @@ source("./global.R")
 #* @get api/v1/raw-observations
 #* @tag Raw
 
-function(res, station = "IDCJDW2124", check = FALSE){
+function(req, res, station = "IDCJDW2124", check = FALSE){
+  
+  # -- Authentication!
+  if (!"HTTP_X_API_KEY" %in% names(req) || req$HTTP_X_API_KEY != APIKEY) {
+    res$body <- "Unauthorized"
+    res$status <- 401
+    return("Unauthorized API call")}
   
   # -- get list of raw files
   observations <- raw_observations(path = path$raw, station = station, check = check)
@@ -44,8 +50,14 @@ function(res, station = "IDCJDW2124", check = FALSE){
 #* @get api/v1/raw-observation
 #* @tag Raw
 
-function(res, station = "IDCJDW2124", year = format(Sys.Date(), "%Y"), 
+function(req, res, station = "IDCJDW2124", year = format(Sys.Date(), "%Y"), 
          month = format(Sys.Date(), "%m"), check = FALSE){
+  
+  # -- Authentication!
+  if (!"HTTP_X_API_KEY" %in% names(req) || req$HTTP_X_API_KEY != APIKEY) {
+    res$body <- "Unauthorized"
+    res$status <- 401
+    return("Unauthorized API call")}
   
   # -- get list of raw files
   observations <- raw_observations(path = path$raw,
@@ -81,7 +93,13 @@ function(res, station = "IDCJDW2124", year = format(Sys.Date(), "%Y"),
 #* @get api/v1/observations
 #* @tag Observations
 
-function(res, station = "IDCJDW2124", start = NA, end = NA){
+function(req, res, station = "IDCJDW2124", start = NA, end = NA){
+  
+  # -- Authentication!
+  if (!"HTTP_X_API_KEY" %in% names(req) || req$HTTP_X_API_KEY != APIKEY) {
+    res$body <- "Unauthorized"
+    res$status <- 401
+    return("Unauthorized API call")}
   
   cat("[API] Call to get observations \n")
   
@@ -112,8 +130,14 @@ function(res, station = "IDCJDW2124", start = NA, end = NA){
 #* @post api/v1/observations
 #* @tag Observations
 
-function(res, station = "IDCJDW2124", year = format(Sys.Date(), "%Y"), month = format(Sys.Date(), "%m"),
+function(req, res, station = "IDCJDW2124", year = format(Sys.Date(), "%Y"), month = format(Sys.Date(), "%m"),
          incremental = TRUE){
+  
+  # -- Authentication!
+  if (!"HTTP_X_API_KEY" %in% names(req) || req$HTTP_X_API_KEY != APIKEY) {
+    res$body <- "Unauthorized"
+    res$status <- 401
+    return("Unauthorized API call")}
   
   cat("[API] Call to import raw observation file \n")
   
@@ -150,7 +174,13 @@ function(res, station = "IDCJDW2124", year = format(Sys.Date(), "%Y"), month = f
 #* @get api/v1/predictions
 #* @tag Predictions
 
-function(res, station = "IDCJDW2124", model = "M1", start = NA, end = NA){
+function(req, res, station = "IDCJDW2124", model = "M1", start = NA, end = NA){
+  
+  # -- Authentication!
+  if (!"HTTP_X_API_KEY" %in% names(req) || req$HTTP_X_API_KEY != APIKEY) {
+    res$body <- "Unauthorized"
+    res$status <- 401
+    return("Unauthorized API call")}
   
   cat("[API] Call to get predictions \n")
   
